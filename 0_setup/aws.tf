93c173f2010ab02b5cd8d76b1fb9d52ce550d60b
Guy Barros <guy@Hashicorp.com>
2024-07-08 15:20:52 +0100

    new role arn

--- 0_setup/aws.tf
+++ 0_setup/aws.tf
@@ -1,76 +1,67 @@
 locals {
-  my_email                   = split("/", data.aws_caller_identity.current.arn)[2]
-  tfc_hostname               = "app.terraform.io"
-  workload_identity_audience = "aws.workload.identity"
+  development_stack_subjects = ["organization:${var.organization_id}:*"]
+  jwt_audience               = "terraform-stacks-private-preview"
 }
 
-data "aws_caller_identity" "current" {}
+# Terraform Cloud production OpenID provider.
+resource "aws_iam_openid_connect_provider" "stacks" {
+  url = "https://app.terraform.io"
 
-data "tls_certificate" "tfc_certificate" {
-  url = "https://${local.tfc_hostname}"
+  client_id_list  = [local.jwt_audience]
+  thumbprint_list = ["9E99A48A9960B14926BB7F3B02E22DA2B0AB7280"]
 }
 
-resource "aws_iam_openid_connect_provider" "tfc_provider" {
-  url             = "https://${local.tfc_hostname}"
-  client_id_list  = [local.workload_identity_audience]
-  thumbprint_list = ["${data.tls_certificate.tfc_certificate.certificates.0.sha1_fingerprint}"]
-}
+# This role is assumed by Terraform Cloud dynamic credentials, accepting any
+# stack that matches the subject string in local.development_stack_subjects.
+resource "aws_iam_role" "stacks" {
+  name = var.role_name
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" = aws_iam_openid_connect_provider.stacks.arn,
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "app.terraform.io:aud" : local.jwt_audience,
+          },
+          "StringLike" : {
+            "app.terraform.io:sub" : local.development_stack_subjects,
+          },
+        },
+      },
+    ],
+  })
 
-data "aws_iam_policy_document" "tfc_role_trust_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.tfc_provider.arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${local.tfc_hostname}:aud"
-      values   = [local.workload_identity_audience]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${local.tfc_hostname}:sub"
-      values = [
-        "organization:${data.tfe_organization.organization.name}:project:${var.project_name}:*"
-      ]
-    }
+  tags = {
+    "Source" = "aws-openid-role-for-stacks"
   }
 }
 
-resource "aws_iam_role" "tfc_role" {
-  name               = "tfc-workload-identity-${local.my_email}"
-  assume_role_policy = data.aws_iam_policy_document.tfc_role_trust_policy.json
+# This policy permits the specified allowed actions, always including the
+# mandatory action to get the caller identity.
+resource "aws_iam_role_policy" "stacks" {
+  name = "stacks"
+  role = aws_iam_role.stacks.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : concat(var.allowed_actions, [
+          "sts:GetCallerIdentity",
+        ]),
+        "Effect" : "Allow",
+        "Resource" : "*"
+      }
+    ]
+  })
 }
 
-# this is the actual policy to do stuff on AWS
-# change it according to your needs
-
-resource "aws_iam_policy" "tfc_policy" {
-  name        = "tfc-policy"
-  description = "TFC run policy"
-
-  policy = <<-EOF
-  {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "aws:*"
-      ],
-      "Resource": "*"
-    }
-  ]
-  }
-  EOF
-}
-
-resource "aws_iam_role_policy_attachment" "tfc_policy_attachment" {
-  role       = aws_iam_role.tfc_role.name
-  policy_arn = aws_iam_policy.tfc_policy.arn
+# Emit the role ARN, for use in the stack configuration.
+output "role_arn" {
+  value = aws_iam_role.stacks.arn
 }
